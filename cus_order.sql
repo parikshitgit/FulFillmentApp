55a8756aa0a2fdb0fbb9cf228d99c12563079041
parikshitgit <38384443+parikshitgit@users.noreply.github.com>
2018-04-20 15:31:08 -0400

    Update cus_order.sql

--- cus_order.sql
+++ cus_order.sql
@@ -1,22 +1,9 @@
--- phpMyAdmin SQL Dump
--- version 4.7.0
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Generation Time: Apr 16, 2018 at 07:10 PM
--- Server version: 5.5.56-log
--- PHP Version: 7.1.4
+
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
 
 --
 -- Database: `cus_order`
