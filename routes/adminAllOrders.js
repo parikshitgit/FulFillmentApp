d5541a37ae365b6cbbf9f4d30c7257e10b0fc930
parikshitgit <38384443+parikshitgit@users.noreply.github.com>
2018-04-20 18:50:03 -0400

    Add files via upload

---
+++ routes/adminAllOrders.js
@@ -0,0 +1,22 @@
+var express = require('express');
+var router = express.Router();
+var mysql = require('mysql');
+var db = require('./database');
+var path = require('path');
+
+
+router.get('/getAllAdminOrders', function(req, res) {
+	
+	var getMyOrders = "SELECT co.order_id,co.order_status,co.order_amount,DATE_FORMAT(co.created_date,'%m/%d/%Y') as 'created_date',ci.item_id as 'product_id',i.item_name as 'product_name' FROM customer_orders as co, order_items as ci,items as i WHERE co.order_id = ci.order_id and ci.item_id = i.id order by co.order_id desc";
+	console.log("getMyOrders========================>"+getMyOrders);
+	db.executeMyQuery(getMyOrders, function(err, results) {
+
+		res.render('adminAllOrders', {
+		    title: 'Orders',
+		    myOrders:results
+		  });
+		
+	});
+});
+
+module.exports = router;
