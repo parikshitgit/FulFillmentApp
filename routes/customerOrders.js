104461ee1a57da53b6f48658d0258d15302b72b6
parikshitgit <38384443+parikshitgit@users.noreply.github.com>
2018-04-20 00:23:18 -0400

    Add files via upload

--- routes/customerOrders.js
+++ routes/customerOrders.js
@@ -5,69 +5,111 @@ var db = require('./database');
 var path = require('path');
 
 router.get('/create', function(req, res) {
-	console.log("========================>>>> create order form")
-	var productItems = "SELECT * FROM items order by id";
-	db.executeMyQuery(productItems, function(err, results) {
 
-		res.render('createOrder', {
-		    title: 'Create New Order',
-		    products:results
-		  });
-		
-	});
-	
-	 
+  var session = req.session;
+  var userId = session.userId;
+
+  
+  if (typeof userId === "undefined") {
+    res.redirect('/');
+  }
+
+  var productItems = "SELECT * FROM items order by id";
+  db.executeMyQuery(productItems, function(err, results) {
+
+    res.render('createOrder', {
+        title: 'Create New Order',
+        products:results
+      });
+    
+  });
+  
+   
 });
 
 router.get('/searchItem', function(req, res) {
-	
-	var searchTerm = req.query.term;
-	//console.log("searchTerm========================>"+searchTerm);
-	var productItems = "SELECT * FROM items WHERE id = '"+searchTerm+"' order by id";
-	//console.log("productItems========================>"+productItems);
-	db.executeMyQuery(productItems, function(err, results) {
-		db.makeResult(req,res,results);
-	});
+  
+  var searchTerm = req.query.term;
+  //console.log("searchTerm========================>"+searchTerm);
+  var productItems = "SELECT * FROM items WHERE id = '"+searchTerm+"' order by id";
+  //console.log("productItems========================>"+productItems);
+  db.executeMyQuery(productItems, function(err, results) {
+    db.makeResult(req,res,results);
+  });
 });
 
 router.get('/myOrders', function(req, res) {
-	var session = req.session;
-	var userId = session.userId;
-	var getMyOrders = "SELECT co.order_id,co.order_status,co.order_amount,DATE_FORMAT(co.created_date,'%d/%m/%Y') as 'created_date',ci.item_id as 'product_id',i.item_name as 'product_name' FROM customer_orders as co, order_items as ci,items as i WHERE co.user_id='"+userId+"' and co.order_id = ci.order_id and ci.item_id = i.id order by co.order_id desc";
-	console.log("getMyOrders========================>"+getMyOrders);
-	db.executeMyQuery(getMyOrders, function(err, results) {
+  var session = req.session;
+  var userId = session.userId;
 
-		res.render('myOrders', {
-		    title: 'My Orders',
-		    myOrders:results
-		  });
-		
-	});
+  console.log("userId========================>"+userId);
+  if (typeof userId === "undefined") {
+    res.redirect('/');
+  }
+
+  var getMyOrders = "SELECT co.order_id,co.order_status,co.order_amount,DATE_FORMAT(co.created_date,'%d/%m/%Y') as 'created_date',ci.item_id as 'product_id',i.item_name as 'product_name' FROM customer_orders as co, order_items as ci,items as i WHERE co.user_id='"+userId+"' and co.order_id = ci.order_id and ci.item_id = i.id order by co.order_id desc";
+  console.log("getMyOrders========================>"+getMyOrders);
+  db.executeMyQuery(getMyOrders, function(err, results) {
+
+    var message = '';
+    var success_message = "";
+    sess = req.session;
+    if (sess.success_alert){
+      success_message = sess.success_alert; 
+      sess.success_alert = ""
+    }
+    
+    res.render('myOrders', {
+        title: 'My Orders',
+        myOrders:results,
+        message: message,
+        success_message:success_message
+        
+      });
+    
+  });
 });
 
 router.post('/placeOrder', function(req, res) {
-	
-	var body = req.body;
-	var session = req.session;
-	var product = body.product;
-	var qty = body.qty;
-	var price = body.price;
-	var userId = session.userId;
-	var orderAmount = (parseInt(qty)*parseFloat(price));
-	var tax = 0;
-	var netAmount = (orderAmount-tax);
-	var datetime = new Date();
-	var placeOrderQuery = "insert into customer_orders (user_id,order_status,order_amount,tax_amount,net_amount) values('"+userId+"','Processing','"+orderAmount+"','"+tax+"','"+netAmount+"')";
-	//console.log("placeOrderQuery========================>"+placeOrderQuery);
-	db.executeMyQuery(placeOrderQuery, function(err, results) {
-		
-		//console.log("results========================>"+JSON.stringify(results));
-		placeOrderQuery = "insert into order_items (item_id,order_id,qty) values('"+product+"','"+results.insertId+"','"+qty+"')";
-		console.log("order_items========================>"+placeOrderQuery);
-		db.executeMyQuery(placeOrderQuery, function(err, results) {
-			res.redirect('/order/myOrders');
-		});
-		
-	});
+  
+  var body = req.body;
+  var session = req.session;
+  var product = body.product;
+  var qty = body.qty;
+  var price = body.price;
+  var userId = session.userId;
+  if (typeof userId === "undefined") {
+    res.redirect('/');
+  }
+
+
+  var orderAmount = price;
+  var tax = 0;
+  var netAmount = (orderAmount-tax);
+  var datetime = new Date();
+  var status = "Processing";
+  if(orderAmount<1000){
+    status = "Approved";
+  }
+  var placeOrderQuery = "insert into customer_orders (user_id,order_status,order_amount,tax_amount,net_amount) values('"+userId+"','"+status+"','"+orderAmount+"','"+tax+"','"+netAmount+"')";
+  //console.log("placeOrderQuery========================>"+placeOrderQuery);
+  db.executeMyQuery(placeOrderQuery, function(err, results) {
+    
+    var orderId = results.insertId;
+    //console.log("results========================>"+JSON.stringify(results));
+    placeOrderQuery = "insert into order_items (item_id,order_id,qty) values('"+product+"','"+orderId+"','"+qty+"')";
+    console.log("order_items========================>"+placeOrderQuery);
+    db.executeMyQuery(placeOrderQuery, function(err, results) {
+      var message = '';
+      
+      var success_message = 'Order #'+orderId+' created successfully'
+      sess = req.session;
+      sess.success_alert; 
+      sess.success_alert=success_message;
+      
+      res.redirect('/order/myOrders');
+    });
+    
+  });
 });
 module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -5,32 +5,38 @@ var db = require('./database');
 var session = require('express-session');
 var sess;
 router.get('/', function(req, res){
-	
-	var message = '';
-	var success_message = "";
+  
+  var message = '';
+  var success_message = "";
     res.render('index', {
-		    title: 'Login Form',
-		    message: message,
-		    success_message:success_message
-		  });
-	  
-	
-	
+        title: 'Login Form',
+        message: message,
+        success_message:success_message
+      });
+    
+  
+  
   
 });
 
 router.get('/signUp', function(req, res){
-	
-	var message = '';
-	var success_message = "";
-	  res.render('sign_up', {
-		    title: 'Sign Up Form',
-		    message: message,
-		    success_message:success_message
-		  });
-	  
-	
-	
+  
+  var message = '';
+  var success_message = "";
+  sess = req.session;
+  if (sess.success_alert){
+    success_message = sess.success_alert; 
+    sess.success_alert = ""
+  }
+  
+    res.render('sign_up', {
+        title: 'Sign Up Form',
+        message: message,
+        success_message:success_message
+      });
+    
+  
+  
 
 });
 
@@ -48,107 +54,108 @@ router.get('/logout',function(req,res){
 }); 
 
 router.post('/addUser', function(req, res){
-	var body = req.body;
-	var first_name = body.firstName
-	var last_name = body.last_name
-	var username = body.loginId
-	var email = body.email
-	var password = body.password
-	var rePassword = body.repassword
-	var profile_type =1;
-	console.log("first_name===================== "+first_name);
-	console.log("email========================= "+email)
-	console.log("password========================= "+password)
-	console.log("rePassword========================= "+rePassword)
-	var sql_find="SELECT * FROM users WHERE username='"+username+"'";
-	var user_find_length =0
-	db.executeMyQuery(sql_find, function(err, results) {
-		console.log("results.length ==========================>"+results.length);
-		
-		
-		
-		if(results.length > 0){
-			user_find_length =1
-			var message = 'User Already Created, Please Enter different Login Id';
-			var success_message = "";
-			  res.render('sign_up', {
-				    title: 'Sign Up Form',
-				    message: message,
-				    success_message:success_message
-				  });
-			  
-		}
-		else
-			{
-				var date = new Date();
-				var sql = "INSERT INTO users (first_name, last_name, email, password,profile_type,username) VALUES ('"+first_name+"', '"+last_name+"', '"+email+"', '"+password+"', '"+profile_type+"', '"+username+"')";
-				console.log("req==============>"+JSON.stringify(req.body));
-				var checkUser = "select * from users where id = '"+body.id+"'";
-				// Check if already exist
-				db.executeMyQuery(sql, function(err, insertResults) {
-					 console.log("Created Successfull "+insertResults);
-					
-					    var message = '';
-						
-						var success_message = 'User Created Successfull ID :'+username
-						
-						
-					 res.redirect('/');
-					
-				});
-			
-			}
-		
-	});
-	
-	console.log("user_find_length ==========================>"+user_find_length);
-	
-	
-	
+  var body = req.body;
+  var first_name = body.firstName
+  var last_name = body.last_name
+  var username = body.loginId
+  var email = body.email
+  var password = body.password
+  var rePassword = body.repassword
+  var profile_type =1;
+  console.log("first_name===================== "+first_name);
+  console.log("email========================= "+email)
+  console.log("password========================= "+password)
+  console.log("rePassword========================= "+rePassword)
+  var sql_find="SELECT * FROM users WHERE username='"+username+"'";
+  var user_find_length =0
+  db.executeMyQuery(sql_find, function(err, results) {
+    console.log("results.length ==========================>"+results.length);
+    
+    
+    
+    if(results.length > 0){
+      user_find_length =1
+      var message = 'User Already Created, Please Enter different Login Id';
+      var success_message = "";
+        res.render('sign_up', {
+            title: 'Sign Up Form',
+            message: message,
+            success_message:success_message
+          });
+        
+    }
+    else
+      {
+        var date = new Date();
+        var sql = "INSERT INTO users (first_name, last_name, email, password,profile_type,username) VALUES ('"+first_name+"', '"+last_name+"', '"+email+"', '"+password+"', '"+profile_type+"', '"+username+"')";
+        console.log("req==============>"+JSON.stringify(req.body));
+        var checkUser = "select * from users where id = '"+body.id+"'";
+        // Check if already exist
+        db.executeMyQuery(sql, function(err, insertResults) {
+           console.log("Created Successfull "+insertResults);
+          
+              var message = '';
+            
+            var success_message = 'User Created Successfully,  ID :'+username
+            sess = req.session;
+            sess.success_alert; 
+            sess.success_alert=success_message;
+            
+           res.redirect('/signUp');
+          
+        });
+      
+      }
+    
+  });
+  
+  console.log("user_find_length ==========================>"+user_find_length);
+  
+  
+  
 });
 
 router.post('/login', function(req, res){
-	var body = req.body;
-	var username = body.username
-	var password = body.password
-	console.log("username========================= "+username)
-	var sql="SELECT * FROM users WHERE username='"+username+"' and password='"+password+"'";
-	
-	db.executeMyQuery(sql, function(err, results) {
-		console.log("results==========================>"+JSON.stringify(results));
-		if(results.length > 0){
+  var body = req.body;
+  var username = body.username
+  var password = body.password
+  console.log("username========================= "+username)
+  var sql="SELECT * FROM users WHERE username='"+username+"' and password='"+password+"'";
+  
+  db.executeMyQuery(sql, function(err, results) {
+    console.log("results==========================>"+JSON.stringify(results));
+    if(results.length > 0){
 
-			sess = req.session;
-			sess.username; 
-			sess.username=req.body.username;
-			sess.userId = results[0].id;
-			console.log("username====================="+sess.username);
-			  
-			var profile_type = results[0].profile_type;
-			if(profile_type === 2){
-				res.redirect('/adminOrders/getOrders');
-			}else
-			res.redirect('/order/myOrders');
+      sess = req.session;
+      sess.username; 
+      sess.username=req.body.username;
+      sess.userId = results[0].id;
+      console.log("username====================="+sess.username);
+        
+      var profile_type = results[0].profile_type;
+      if(profile_type === 2){
+        res.redirect('/adminOrders/getOrders');
+      }else
+      res.redirect('/order/myOrders');
 
-		
-			}else{
-				console.log("User not found ====================="+results.length);
-				var message = '';
-				message = 'Wrong User Credentials.';
-				var success_message = ''
-				res.render('index', {
-				    title: 'Login Form',
-				    message: message,
-				    success_message:success_message
-				  });
-				
-		  }
-		});
-	
+    
+      }else{
+        console.log("User not found ====================="+results.length);
+        var message = '';
+        message = 'Wrong User Credentials.';
+        var success_message = ''
+        res.render('index', {
+            title: 'Login Form',
+            message: message,
+            success_message:success_message
+          });
+        
+      }
+    });
+  
 });
 
 
 
 
 module.exports = router;
-
