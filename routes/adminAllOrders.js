6d07bc2dfe31b06175eff372a6e284d3538feeb3
parikshitgit <38384443+parikshitgit@users.noreply.github.com>
2018-04-20 19:24:33 -0400

    Update adminAllOrders.js

--- routes/adminAllOrders.js
+++ routes/adminAllOrders.js
@@ -5,7 +5,7 @@ var db = require('./database');
 var path = require('path');
 
 
-router.get('/order/getAllAdminOrders', function(req, res) {
+router.get('/getAllAdminOrders', function(req, res) {
 	
 	var getMyOrders = "SELECT co.order_id,co.order_status,co.order_amount,DATE_FORMAT(co.created_date,'%m/%d/%Y') as 'created_date',ci.item_id as 'product_id',i.item_name as 'product_name' FROM customer_orders as co, order_items as ci,items as i WHERE co.order_id = ci.order_id and ci.item_id = i.id order by co.order_id desc";
 	console.log("getMyOrders========================>"+getMyOrders);
